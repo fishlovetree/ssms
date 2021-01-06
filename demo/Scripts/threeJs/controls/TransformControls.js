b2eaa3a47a71f1716ad9dda7c2cd69a1bc33e11d
fishlovetree <heyunche@sina.com>
2021-01-06 12:59:09 +0800

    Signed-off-by: fishlovetree <heyunche@sina.com>

--- demo/Scripts/threeJs/controls/TransformControls.js
+++ demo/Scripts/threeJs/controls/TransformControls.js
@@ -972,7 +972,7 @@
 
 					if ( scope.axis === "XYZ" ) {
 
-						scale = 1 + ( ( point.y ) / Math.max( oldScale.x, oldScale.y, oldScale.z ) );
+						scale = 1 + ( ( point.y ) / (2 * Math.max( oldScale.x, oldScale.y, oldScale.z )) );
 
 						scope.object.scale.x = oldScale.x * scale;
 						scope.object.scale.y = oldScale.y * scale;
@@ -982,9 +982,9 @@
 
 						point.applyMatrix4( tempMatrix.getInverse( worldRotationMatrix ) );
 
-						if ( scope.axis === "X" ) scope.object.scale.x = oldScale.x * ( 1 + point.x / oldScale.x );
-						if ( scope.axis === "Y" ) scope.object.scale.y = oldScale.y * ( 1 + point.y / oldScale.y );
-						if ( scope.axis === "Z" ) scope.object.scale.z = oldScale.z * ( 1 + point.z / oldScale.z );
+						if (scope.axis === "X") scope.object.scale.x = oldScale.x * (1 + point.x / (2 * oldScale.x));
+						if ( scope.axis === "Y" ) scope.object.scale.y = oldScale.y * ( 1 + point.y / (2 * oldScale.y) );
+						if ( scope.axis === "Z" ) scope.object.scale.z = oldScale.z * ( 1 + point.z / (2 * oldScale.z) );
 
 					}
 
